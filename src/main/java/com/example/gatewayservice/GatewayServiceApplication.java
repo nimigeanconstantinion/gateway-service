713bd9130aa2388a65bbd3dca1717f55f37beca1
nimigeanconstantinion <constantin.nimigean@gmail.com>
2025-12-16 11:45:41 +0200

    am pus @EnableConfigurationProperties(CorsProperties.class) la mainclass

--- src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -1,14 +1,18 @@
 package com.example.gatewayservice;
 
 
+import com.example.gatewayservice.config.CorsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
 
 @SpringBootApplication
+@EnableConfigurationProperties(CorsProperties.class)
+
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
